eaa2c9eebc55727276d5ca1ded3c9baa3ae51d53
Bubfusion <thebubfusion@gmail.com>
2019-01-07 10:20:05 -0500

    checking system + win output

--- penny-pitch.cpp
+++ penny-pitch.cpp
@@ -9,6 +9,12 @@ int main()
   char prizes[5][5];
   char pennies[5][5];
 
+  int cashPrize;
+  int popcornPrize;
+  int foxPrize;
+  int hamburgerPrize;
+  int airpodPrize;
+  int totalWins;
 
   int row,col;
   int num = 0;
@@ -78,7 +84,7 @@ int main()
                   }
               }
           num = 0;
-          
+
     while(num < 10)
       {
         row = rand()%5;
@@ -89,7 +95,7 @@ int main()
 
 }
   num = 0;
-
+cout << "Penny Board" << endl;
   for (int i = 0; i < 5; i++)
 {
   for(int j = 0; j < 5; j++)
@@ -98,7 +104,9 @@ int main()
   }
   cout << endl;
 }
+  cout << endl;
 
+  cout << "Prize Board" << endl;
     for (int i = 0; i < 5; i++)
   {
     for(int j = 0; j < 5; j++)
@@ -106,6 +114,98 @@ int main()
       cout << setw(2) << prizes[i][j];
     }
     cout << endl;
+}
+cout << endl;
+
+for (int i = 0; i < 5; i++)
+{
+  for(int j = 0; j < 5; j++)
+  {
+    if(prizes[i][j] == '$' && pennies[i][j] == '0')
+    {
+      cashPrize = cashPrize + 1;
+    }
+  }
+}
+
+for (int i = 0; i < 5; i++)
+{
+  for(int j = 0; j < 5; j++)
+  {
+    if(prizes[i][j] == 'P' && pennies[i][j] == '0')
+    {
+      popcornPrize = popcornPrize + 1;
+    }
+  }
+}
+
+for (int i = 0; i < 5; i++)
+{
+  for(int j = 0; j < 5; j++)
+  {
+    if(prizes[i][j] == 'F' && pennies[i][j] == '0')
+    {
+      foxPrize = foxPrize + 1;
+    }
+  }
+}
+
+for (int i = 0; i < 5; i++)
+{
+  for(int j = 0; j < 5; j++)
+  {
+    if(prizes[i][j] == 'H' && pennies[i][j] == '0')
+    {
+      hamburgerPrize = hamburgerPrize + 1;
+    }
+  }
+}
+
+for (int i = 0; i < 5; i++)
+{
+  for(int j = 0; j < 5; j++)
+  {
+    if(prizes[i][j] == 'A' && pennies[i][j] == '0')
+    {
+      airpodPrize = airpodPrize + 1;
+    }
+  }
+}
+
+  if(cashPrize == 3)
+{
+  cout << "You won $5!" << endl;
+  totalWins = totalWins + 1;
+}
+
+  if(popcornPrize == 3)
+{
+  cout << "You won a large Popcorn!" << endl;
+  totalWins = totalWins + 1;
+}
+
+  if(foxPrize == 3)
+{
+  cout << "You literally won a fox. Wack" << endl;
+  totalWins = totalWins + 1;
+}
+if (hamburgerPrize == 3)
+{
+  cout << "You won a hamburger!" << endl;
+  totalWins = totalWins + 1;
+}
+if (airpodPrize == 3)
+{
+  cout << "You won aipods. Now you can flex on them brokes" << endl;
+  totalWins = totalWins + 1;
+}
+
+if (totalWins == 0) {
+  cout << "You won nothin. Better luck next time." << endl;
+}
+if (totalWins == 5)
+{
+  cout << "Alright buddy calm down. You won popcorn, a hamburger, a fox, airpods, and $5. Thats pretty crazy" << endl;
 }
   return 0;
 }
